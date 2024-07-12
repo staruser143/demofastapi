57be1140525b522a4dc565a457a8c02da4010ace
staruser143 <seriesfan.in@gmail.com>
2024-07-12 16:00:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from models import customers, create_tables, User, Address
+from db_operations import fetch_users_with_pagination,insert_records, fetch_records, fetch_users_with_addresses,execute_raw_sql
+
 app = FastAPI()
 
 
@@ -12,3 +15,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/records")
+def get_records():
+    return fetch_records()
